86246b92a90ca96749b2a9e246f29c2219824633
NataliiaSwan <148957684+NataliiaSwan@users.noreply.github.com>
2024-03-15 14:00:02 +0200

    add changes task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,59 +1,31 @@
-const input = document.querySelector('.controls-input');
-const create = document.querySelector('[data-create]');
-const destroy = document.querySelector('[data-destroy]');
-const boxesContainer = document.querySelector('#boxes');
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-let width = 30;
-let height = 30;
-createButton.addEventListener('click', () => {
-  if (input.value < 1 || input.value > 100) {
-    return;
+const refs = {
+  amountEl: document.querySelector('#controls input'),
+  createEl: document.querySelector('button[data-create]'),
+  destroyEl: document.querySelector('button[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+};
+refs.createEl.addEventListener('click', onCreateClick);
+refs.destroyEl.addEventListener('click', onDestroyClick);
+function onCreateClick() {
+  let amount = refs.amountEl.value;
+  if (amount < 1 || amount > 100) {
+    return alert(
+      'Ooops, something go wrong!!! The value must be less than 100'
+    );
   }
-});
-createButton(input, value);
-destroyButton.addEventListener('click', () => {
-  boxesContainer.innerHTML = '';
-  input.value = '';
-});
+  onDestroyClick();
+  createBoxes(amount);
+}
+function onDestroyClick() {
+  refs.boxesEl.innerHTML = '';
+  refs.amountEl.value = '';
+}
 function createBoxes(amount) {
   for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    box.style.width = 30;
-    box.style.height = 30;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-    size += 10;
+    let elem = document.createElement('div');
+    elem.style.width = 30 + 10 * i + 'px';
+    elem.style.height = 30 + 10 * i + 'px';
+    elem.style.backgroundColor = getRandomHexColor();
+    refs.boxesEl.append(elem);
   }
 }
-
-// Напиши скрипт створення й очищення колекції елементів з наступним функціоналом.
-// Є input, у який користувач вводить бажану кількість елементів.
-//  Після натискання на кнопку Create має рендеритися(додаватися в DOM)
-//   колекція з відповідною кількістю елементів і очищатися значення в інпуті.
-//   При повторному натисканні на кнопку Create поверх старої колекції має
-//    рендеритись нова.Після натискання на кнопку Destroy колекція елементів має очищатися.
-//    Після натискання користувачем на кнопку Create треба провалідувати значення в input,
-//   воно має бути в межах від 1 до 100 включно.Тільки якщо воно задоволяє умову, мають
-//    додаватися нові < div > елементи в DOM.
-
-// Для рендеру елементів на сторінці створи функцію createBoxes(amount), яка приймає
-// один параметр — число, що зберігає кількість елементів для рендеру.
-
-// Функція має створювати стільки < div > елементів, скільки вказано в параметрі amount
-//  і додавати їх у DOM дочірніми елементами для div#boxes.
-
-// Розміри першого <div> елемента мають бути 30px на 30px.
-// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
-// Усі елементи повинні мати випадковий колір фону.Використовуй готову
-// функцію getRandomHexColor() для отримання випадкового кольору.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// Для очищення колекції після натискання на кнопку Destroy створи функцію destroyBoxes(),
-//   яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
